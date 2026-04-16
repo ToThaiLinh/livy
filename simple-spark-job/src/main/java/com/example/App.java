28b6b5bcfbf8071c985463193f5865dec1270736
To Thai Linh <tothailinhytbg2003@gmail.com>
2026-04-17 03:18:44 +0700

    add Dockerfile livy

---
+++ simple-spark-job/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i = 0; i < n; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Hello World!");
+    }
+}
